9049600609eb670b34c8711bc1d56e70d485f15a
Andrew <jaknowles1997@gmail.com>
2018-04-09 08:00:41 -0700

    server.js boilerplate code added

---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/brewery-api.js")(app);
+require("./routes/beer-api.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});

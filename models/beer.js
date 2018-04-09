db3889bbcc33a5eeba717cb75340e151acd04127
brad ferrell <ferellbh@gmail.com>
2018-04-09 12:59:40 -0400

    updated beer.js

--- models/beer.js
+++ models/beer.js
@@ -1,36 +1,36 @@
 module.exports = function(sequelize, DataTypes) {
-    var Beer = sequelize.dfine("Beer", {
-        beer_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        abv: {
-            type: DataTypes.FLOAT,
-            allowNull: false
-        },
-        ibu: {
-            type: DataTypes.INTERGER,
-            allowNull: false
-        },
-        style: {
-            type: datatypes.STRING,
-            allowNull: false
-        },
-        cheers: {
-            type: datatypes.INTERGER,
-            defaultValue: 0,
-            allowNull: false
-        },
-        rating: {
-            type: datatypes.INTERGER,
-        }
-    });
-    Beer.associate = function(models) {
-        Beer.belongsTo(models.Brewery, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
+  var Beer = sequelize.define("Beer", {
+    beer_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    abv: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    ibu: {
+      type: DataTypes.INTERGER,
+      allowNull: false
+    },
+    style: {
+      type: datatypes.STRING,
+      allowNull: false
+    },
+    cheers: {
+      type: datatypes.INTERGER,
+      defaultValue: 0,
+      allowNull: false
+    },
+    rating: {
+      type: datatypes.INTERGER
     }
-    return Beer;
-}
+  });
+  Beer.associate = function(models) {
+    Beer.belongsTo(models.Brewery, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+  return Beer;
+};

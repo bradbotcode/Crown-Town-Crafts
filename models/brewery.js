083de7d6e86e7d292120e90ebf5199f51dfbe177
brad ferrell <ferellbh@gmail.com>
2018-04-09 11:55:44 -0400

    updating brewery model to join with beer table

--- models/brewery.js
+++ models/brewery.js
@@ -1,32 +1,40 @@
 module.exports = function(sequelize, DataTypes) {
-    var Brewery = sequelize.define("Brewery", {
-        brewery_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        neighborhood: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        year_est: {
-            type: DataTypes.INTERGER,
-            $lte: 9999,
-            allowNull: false
-        },
-        desc: {
-            type: DataTypes.STRING
-        },
-        website: {
-            type: DataTypes.STRING
-        },
-        street: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        zip: {
-            type: DataTypes.INTERGER
-        }
+  var Brewery = sequelize.define("Brewery", {
+    brewery_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    neighborhood: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    year_est: {
+      type: DataTypes.INTERGER,
+      $lte: 9999,
+      allowNull: false
+    },
+    desc: {
+      type: DataTypes.STRING
+    },
+    website: {
+      type: DataTypes.STRING
+    },
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    zip: {
+      type: DataTypes.INTERGER
+    }
+  });
 
+  Brewery.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Author.hasMany(models.Beer, {
+      onDelete: "cascade"
     });
-    return Brewery;
-}
+  };
+
+  return Brewery;
+};

f99e5244feb190e166540b86c269cfec005dcecf
brad ferrell <ferrellbh@gmail.com>
2018-06-15 18:34:37 -0400

    removed handlebars

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
 
 // sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // requiring our models for syncing
-var db = require("./models");
+const db = require("./models");
 
 app.use(
   bodyParser.urlencoded({
@@ -21,16 +21,7 @@ app.use(bodyParser.json());
 // static directory
 app.use(express.static("./public"));
 
-//handlebars engine
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
-var routes = require("./controllers/controller.js");
+const routes = require("./controllers/controller.js");
 app.use("/", routes);
 
 // syncing our sequelize models and then starting our Express app
@@ -39,8 +30,8 @@ db.sequelize
   .sync({
     force: false
   })
-  .then(function () {
-    app.listen(PORT, function () {
+  .then(function() {
+    app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
   });
